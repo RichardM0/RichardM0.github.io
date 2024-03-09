68926cf98db618af33a31f9afb96da9c8dc5d5b3
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2024-03-09 18:07:25 -0500

    fix size not looking great on mobile

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function myFunction() {
     var x = document.getElementById("top-navbar-right");
     var i = document.getElementById("icon-symbol");
     var c = document.getElementById("icon-toggle");
+    var collection = document.getElementsByClassName("navbar-right-link");
+    var items = Array.prototype.slice.call( collection, 0 );
     if (x.className === "navbar-sect navbar-right") {
         x.className = "vertical-navbar";
-        var collection = document.getElementsByClassName("navbar-right-link");
-        var items = Array.prototype.slice.call( collection, 0 );
         i.className = "fa fa-angle-up";
         c.style.display = "flex";
         c.style.justifyContent = "center";
@@ -19,5 +19,10 @@ function myFunction() {
         x.className = "navbar-sect navbar-right";
         i.className = "fa fa-bars";
         c.style.display = "block";
+        for(let i=0; i<items.length; i++){
+            console.log(items[i]);
+            items[i].style.visibility = "hidden";
+            items[i].pointerEvents = "none";
+        }
     }
 }
