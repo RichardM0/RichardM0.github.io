72714c8f7ecb7ad8662ac2db0e39f240892e1a1c
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2024-04-04 22:35:47 -0400

    bootstrap, add photos + fix dropdown menu bug

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 function myFunction() {
-    var x = document.getElementById("top-navbar-right");
+    var x = document.getElementById("mytop-navbar-right");
     var i = document.getElementById("icon-symbol");
     var c = document.getElementById("icon-toggle");
-    var collection = document.getElementsByClassName("navbar-right-link");
-    var items = Array.prototype.slice.call( collection, 0 );
-    if (x.className === "navbar-sect navbar-right") {
-        x.className = "vertical-navbar";
-        x.style.height = "90%";
+
+    if (x.className === "mynavbar-sect mynavbar-right") {
+        var collection = document.getElementsByClassName("mynavbar-right-link");
+        var items = Array.prototype.slice.call( collection, 0 );
+        x.className = "myvertical-navbar";
+        document.getElementById("RM-logo").style.visibility = "hidden";
+        x.style.height = "100%";
         i.className = "fa fa-angle-up";
+        i.style.margin = "1% 2% 0 0";
         c.style.display = "flex";
         c.style.justifyContent = "center";
         for(let i=0; i<items.length; i++){
-            console.log(items[i]);
-            items[i].style.visibility = "visible";
-            items[i].pointerEvents = "auto";
-                
+            /*items[i].style.visibility = "visible";
+            items[i].pointerEvents = "auto"; */
+            items[i].className = "mynavbar-right-link-vert"
         }
       
     } else {
-        x.className = "navbar-sect navbar-right";
-        x.style.height = "0";
+        var collection = document.getElementsByClassName("mynavbar-right-link-vert");
+        var items = Array.prototype.slice.call( collection, 0 );
+        document.getElementById("RM-logo").style.visibility = "visible";
+        x.className = "mynavbar-sect mynavbar-right";
         i.className = "fa fa-bars";
         c.style.display = "block";
         for(let i=0; i<items.length; i++){
             console.log(items[i]);
-            if(window.innerWidth<900){
-                items[i].style.visibility = "hidden";
-                items[i].pointerEvents = "none";  
-            }
+            /*items[i].style.visibility = "hidden";
+            items[i].pointerEvents = "none";  */
+            items[i].className = "mynavbar-item mynavbar-right-link"
         }
     }
 }
